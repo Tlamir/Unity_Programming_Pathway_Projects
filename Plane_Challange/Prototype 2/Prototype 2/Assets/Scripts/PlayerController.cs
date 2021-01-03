ded6f4b2e229ed096a468fcda115c949bb424a1c
Yigithan Guven <demirejderha@gmail.com>
2021-01-03 23:23:25 +0300

    Lesson 2.1
    Lesson 2.1 programming pathway

---
+++ Plane_Challange/Prototype 2/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizonralInput;
+    public float verticalInput;
+    public float speed = 10.0f;
+    public float xRange = 10.0f;
+    public float zRange = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Keep Player in bounds
+        if (transform.position.x<-xRange)//Right Boundary
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x >xRange)//Left Boundary
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        horizonralInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * horizonralInput * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime*speed);
+    }
+}
