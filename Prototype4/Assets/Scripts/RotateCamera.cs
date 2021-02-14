434a92f87df9ec87cec4dc9728f779b89a09ef88
Yigithan Guven <demirejderha@gmail.com>
2021-02-15 02:37:21 +0300

    Lesson 4.1 Camera Control

---
+++ Prototype4/Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    public float rotationSpeed = 50.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontaInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up,horizontaInput*rotationSpeed*Time.deltaTime);
+    }
+}
