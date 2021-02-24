88fe63e2eecf50da9cb031b6b912a2020d82efd6
Yigithan Guven <demirejderha@gmail.com>
2021-02-24 03:55:08 +0300

    Lesson 4.3 PowerUp and CountDown

--- Prototype4/Assets/Scripts/PlayerController.cs
+++ Prototype4/Assets/Scripts/PlayerController.cs
@@ -7,7 +7,10 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     private GameObject focalPoint;
     private Rigidbody playerRb;
+    public GameObject powerupIndicator;
     public float speed = 5.0f;
+    public bool hasPowerUp = false;
+    private float powerupStrenght = 15.0f;
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
@@ -19,6 +22,35 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         float forwardInput = Input.GetAxis("Vertical");
+
         playerRb.AddForce(focalPoint.transform.forward*speed*forwardInput);
+
+        powerupIndicator.transform.position = transform.position+ new Vector3(0,-0.5f,0);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerUp = true;
+            powerupIndicator.gameObject.SetActive(true);
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+        }
+    }
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerUp = false;
+        powerupIndicator.gameObject.SetActive(false);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy")&&hasPowerUp)
+        {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = collision.gameObject.transform.position-transform.position;     
+            enemyRigidbody.AddForce(awayFromPlayer * powerupStrenght, ForceMode.Impulse);
+        }
     }
 }
+ 
