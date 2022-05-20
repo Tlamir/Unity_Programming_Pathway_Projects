db5214d92b58329c8427d396c366b395ac66e5f0
Yigithan Guven <demirejderha@gmail.com>
2022-05-20 15:33:42 +0300

    5.2 keeping score

--- Prototype 5/Assets/Scripts/GameManager.cs
+++ Prototype 5/Assets/Scripts/GameManager.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
+    private int _score;
+    public TextMeshProUGUI scoreText;
     private float spawnRate = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnTarget());
+        _score = 0;
     }
 
     // Update is called once per frame
@@ -25,5 +29,11 @@ public class GameManager : MonoBehaviour
             int index= Random.Range(0, targets.Count);
             Instantiate(targets[index]);
         }
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        _score += scoreToAdd;
+        scoreText.text = "Score: " + _score;
     }
 }
--- Prototype 5/Assets/Scripts/Target.cs
+++ Prototype 5/Assets/Scripts/Target.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     private Rigidbody targerRb;
+    private GameManager gameManager;
     private float minSpeed = 12;
     private float maxSpeed = 16;
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -2;
+    public int pointValue;
+    public ParticleSystem explosionPaticle;
     // Start is called before the first frame update
     void Start()
     {
         targerRb = GetComponent<Rigidbody>();
+        gameManager=GameObject.Find("GameManager").GetComponent<GameManager>();
         targerRb.AddForce(RandomForce(),ForceMode.Impulse);
         targerRb.AddTorque(RandomTourqe(),RandomTourqe(), RandomTourqe(), ForceMode.Impulse);
         transform.position = RandomSpawnPos();
@@ -27,7 +31,10 @@ public class Target : MonoBehaviour
 
     private void OnMouseDown()
     {
+
         Destroy(gameObject);
+        Instantiate(explosionPaticle,transform.position,explosionPaticle.transform.rotation);
+        gameManager.UpdateScore(pointValue);
     }
 
     private void OnTriggerEnter(Collider other)
